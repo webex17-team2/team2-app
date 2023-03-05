6d5c83ce3853c0e5b3ed858fbb597d3dc4242f3a
Renmurata <tmjuit@gmail.com>
2023-03-05 20:10:21 +0900

    firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// 必要な関数を import
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEW4PHztDdaeNVjIb4tAC5zh0JCDthpMo",
+    authDomain: "team2-app.firebaseapp.com",
+    projectId: "team2-app",
+    storageBucket: "team2-app.appspot.com",
+    messagingSenderId: "1061567398770",
+    appId: "1:1061567398770:web:fb1c5fdd29f832b63e13eb",
+    measurementId: "G-S1T4B58DGR",
+}
+
+// Firebaseアプリオブジェクトを初期化
+const app = initializeApp(firebaseConfig)
+    // Firestoreを読み込み、db(databaseの略)として export
+export const db = getFirestore(app)
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import "./firebase.js" // これを追加
 
 createApp(App).mount("#app")
