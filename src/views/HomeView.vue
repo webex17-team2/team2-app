01368c370eb02531e8cd3d377f71e6f22b3c3395
yoshihitoishihara <yoshihito2103@outlook.jp>
2023-03-08 01:54:47 +0900

    投稿フォームの見た目を実装

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home__wrapper">
+    <submit-form />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-}
+  import SubmitForm from "../components/SubmitForm.vue"
+  export default {
+    components: { SubmitForm },
+  }
 </script>
+
+<style scoped>
+  .home__wrapper {
+    margin: 0 auto;
+    max-width: 600px;
+    background-color: #ccc;
+  }
+</style>
