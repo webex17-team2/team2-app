ea6a6baed4684e53f67de2d0c12451965acd6801
kujiKazuaki <KujiKazuaki24@gmail.com>
2023-03-14 09:20:19 +0900

    エラー解消

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,9 @@ import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 import MyPageView from "../views/MyPageView.vue"
-<<<<<<< HEAD
 import JapanView from "../views/JapanView.vue"
-=======
 import ListOfPostsView from "../views/ListOfPostsView.vue"
 import AddPostsView from "../views/AddPostsView.vue"
->>>>>>> 4462685315c0a1f13532b851cf306cae6ab6ac0a
 
 const routes = [
   {
@@ -25,16 +22,13 @@ const routes = [
     name: "MyPage",
     component: MyPageView,
   },
-<<<<<<< HEAD
   {
     path: "/japan-page",
     name: "Japan",
     component: JapanView,
   },
-=======
   { path: "/listOfPosts", name: "ListOfPosts", component: ListOfPostsView },
   { path: "/addPostsView", name: "AddPostsView", component: AddPostsView },
->>>>>>> 4462685315c0a1f13532b851cf306cae6ab6ac0a
 ]
 
 const router = createRouter({
