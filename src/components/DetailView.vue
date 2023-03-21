97c1700c1b55bdfcf3bb408b98f36434cf1c2d01
MiyuMizuno <miyu.leon.1431@icloud.com>
2023-03-21 12:12:52 +0900

    途中

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -23,6 +23,9 @@
   ></textarea>
 </template>
 <script>
+import { collection, query, where, getDocs } from "firebase/firestore"
+import { ref, getDownloadURL } from "firebase/storage"
+import { db, storage } from "../firebase.js"
 export default {
   //ListOfView.vueから受け取り
   props: {
@@ -55,6 +58,32 @@ export default {
     // const data = this.$router
     // const nameQuery = query(citiesRef, where("タイムスタンプ", "==", "受け取った値"));
     //
+  },
+  methods: {
+    async Read() {
+      const timeQuery = query(
+        collection(db, "posts"),
+        where("timestamp", "==", this.timestamp)
+      )
+      const querySnapshot = await getDocs(timeQuery)
+      querySnapshot.forEach(async (doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        let postdata = doc.data()
+        for (let i = 0; i < doc.data().imgPath.length; i++) {
+          const imgUrl = await getDownloadURL(
+            ref(storage, `files/${doc.data().imgPath[i]}`)
+          ).then((url) => {
+            return url
+          })
+          postdata.imgPath[i] = imgUrl
+        }
+        this.postArray.push(postdata)
+      })
+      console.log("Array")
+      console.log(this.postArray)
+      console.log("aaaaaaa")
+      //console.log(doc.id, " => ", doc.data())
+    },
   },
 }
 </script>
